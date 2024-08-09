2cb24cf9f4405e859b0da1fdf29ac40d5a60f5e9
Jvictorrios <Jvictorrios.ofc@gmail.com>
2024-08-09 13:29:21 -0300

    att1

--- Primeiros Passos/Avt1.sql
+++ Primeiros Passos/Avt1.sql
@@ -10,14 +10,3 @@ create table Clientes (
     membro boolean not null -- boolean: só pode ser true ou false
  )
  
- create table Clientes (
-    ID_Cliente int (2) primary key,  --primary key: identificador 
-    nome varchar (30),
-    Contato int (9),
-    rua varchar (30),
-    numero int (4),         -- int: se limita apenas para números
-    bairro varchar (30),
-    cidade varchar (30), 
-    estado varchar (30),    -- varchar: espaço de texto, números ou caracteres 
-    membro boolean not null -- boolean: só pode ser true ou false
- )
